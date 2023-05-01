c20ef496eb0ec9e96ced7ee219f041bd3f5969fd
Jimmy Vaniski <Vaniskito@hotmail.com>
2023-05-01 22:43:08 +0100

    criando, html, e css  e database

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tarefas.db'
+db = SQLAlchemy(app)  # aponta para a base de dados
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
